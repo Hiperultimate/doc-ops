62bcd74086217635430be083ceb182bc992f9e8b
Hiperultimate <hiperultimatevegito@gmail.com>
2021-09-03 23:19:42 +0530

    Organized init

---
+++ client/src/pages/home/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import TheLifeSavers from "../../svgs/The Lifesavers One on One.svg";
+
+function Home(){
+    return(
+        <div>
+            Home page component.
+            <img src={TheLifeSavers} alt="" />
+        </div>
+    );
+}
+
+
+export default Home;
