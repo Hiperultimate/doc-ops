411fd02aa2c879b2c215e2dd43a46142a5ddc529
Hiperultimate <hiperultimatevegito@gmail.com>
2021-09-04 21:37:04 +0530

    Created main content container

---
+++ client/src/components/mainContainer/MainContainer.jsx
@@ -0,0 +1,14 @@
+import "./mainContainer.css";
+
+function MainContainer(){
+    return(
+        <div className="back-board global-box-shadow">
+            <div className="added-item">Main Container</div>
+            <div className="added-item">Main Container</div>
+            <div className="added-item">Main Container</div>
+            <div className="added-item">Main Container</div>
+        </div>
+    );
+}
+
+export default MainContainer;
--- client/src/pages/doctorAbout/DoctorAbout.jsx
+++ client/src/pages/doctorAbout/DoctorAbout.jsx
@@ -1,27 +1,20 @@
 import "./doctorAbout.css";
 import Navbar from "../../components/navbar/Navbar.jsx";
 import Footer from "../../components/footer/Footer.jsx";
+import MainContainer from "../../components/mainContainer/MainContainer.jsx";
+import MainHeading from "../../components/mainHeading/MainHeading.jsx";
 
-function DoctorAbout(){
-    return(
-        <div class="doctor-about">
-            <Navbar/>
-            Doctor About Page 
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <Footer />
-        </div>
-    )
+function DoctorAbout() {
+  return (
+    <div className="doctor-about">
+      <Navbar />
+      <MainHeading titleName={"About"} />
+      <div className="main-container">
+        <MainContainer />
+      </div>
+      <Footer />
+    </div>
+  );
 }
 
 export default DoctorAbout;
