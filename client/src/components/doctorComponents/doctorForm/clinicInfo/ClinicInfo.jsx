da27c6ae579acf3ffc37a3417a4620b6ec9351b7
Hiperultimate <hiperultimatevegito@gmail.com>
2021-09-12 22:01:40 +0530

    Did some styling on form page

--- client/src/components/doctorComponents/doctorForm/clinicInfo/ClinicInfo.jsx
+++ client/src/components/doctorComponents/doctorForm/clinicInfo/ClinicInfo.jsx
@@ -1,65 +1,99 @@
 import "./clinicInfo.css";
 import InputField from "../../../inputField/InputField.jsx";
 import MainButton from "../../../mainButton/MainButton.jsx";
+import DisplayMap from "../../../displayMap/DisplayMap.jsx";
 
 function ClinicInfo() {
   return (
     <div className="clinic-info global-box-shadow">
-      <InputField
-        wrapperClass={"input-dimention"}
-        heading={"Name"}
-        placeholder={"Enter clinic's name"}
-        type={"text"}
-        fieldName={"clinic-name"}
-      />
-      <InputField
-        wrapperClass={"input-dimention"}
-        heading={"Address"}
-        placeholder={"Enter address"}
-        type={"text"}
-        fieldName={"clinic-address"}
-      />
-      <InputField
-        wrapperClass={"input-dimention"}
-        heading={"Consultation Fee"}
-        placeholder={"Enter consultation fee"}
-        type={"text"}
-        fieldName={"consultation-fee"}
-      />
-      <InputField
-        wrapperClass={"input-dimention"}
-        heading={"Online Consultation"}
-        placeholder={"This will be a drop down field"}
-        type={"text"}
-        fieldName={"online-consultation"}
-      />
-      <InputField
-        wrapperClass={"input-dimention"}
-        heading={"Treatments Offered"}
-        placeholder={"this will be a multi select field"}
-        type={"text"}
-        fieldName={"doctor-treatments"}
-      />
-      <InputField
-        wrapperClass={"input-dimention"}
-        heading={"Specializations"}
-        placeholder={"this will be a multi select field"}
-        type={"text"}
-        fieldName={"doctor-specializations"}
-      />
-      <div className="opening-hours">
-        <span className="btn-left-text">Opening Hours</span>
-        <MainButton
-          buttonText="Enter Opening Hours"
-          arrow={true}
-          onClickHander
-        />
-      </div>
-      <div className="opening-hours">
-        <span className="btn-left-text">Clinic Pictures</span>
-        {/* <MainButton buttonText="Upload" onClickHander /> */}
-        <label htmlFor="myfile">Select Images</label> 
-        <input type="file" className="file-input" id="myfile" name="myfile"/>
+      <div className="map-grid">
+        <div className="clinic-form-part">
+          <div className="clinic-grid">
+            <div className="clinic-name">
+              <InputField
+                wrapperClass={"input-dimention"}
+                heading={"Name"}
+                placeholder={"Enter clinic's name"}
+                type={"text"}
+                fieldName={"clinic-name"}
+              />
+            </div>
+            <div className="clinic-address">
+              <InputField
+                wrapperClass={"input-dimention"}
+                heading={"Address"}
+                placeholder={"Enter address"}
+                type={"text"}
+                fieldName={"clinic-address"}
+              />
+            </div>
+            <div className="clinic-consultation-fee">
+              <InputField
+                wrapperClass={"input-dimention"}
+                heading={"Consultation Fee"}
+                placeholder={"Enter consultation fee"}
+                type={"text"}
+                fieldName={"consultation-fee"}
+              />
+            </div>
+            <div className="clinic-online-consultation">
+              <InputField
+                wrapperClass={"input-dimention"}
+                heading={"Online Consultation"}
+                placeholder={"This will be a drop down field"}
+                type={"text"}
+                fieldName={"online-consultation"}
+              />
+            </div>
+            <div className="clinic-treatments-offered">
+              <InputField
+                wrapperClass={"input-dimention"}
+                heading={"Treatments Offered"}
+                placeholder={"this will be a multi select field"}
+                type={"text"}
+                fieldName={"doctor-treatments"}
+              />
+            </div>
+            <div className="clinic-specialization">
+              <InputField
+                wrapperClass={"input-dimention"}
+                heading={"Specializations"}
+                placeholder={"this will be a multi select field"}
+                type={"text"}
+                fieldName={"doctor-specializations"}
+              />
+            </div>
+            <div className="clinic-opening-hours">
+              <div className="opening-hours-form">
+                <span className="btn-left-text">Opening Hours</span>
+                <MainButton
+                  buttonText="Enter Opening Hours"
+                  arrow={true}
+                  btnStyle={{ width: "200px" , marginLeft: "7px"}}
+                  onClickHander
+                />
+              </div>
+            </div>
+            <div className="clinic-pictures">
+              <div className="clinic-pictures-form">
+                <span className="btn-left-text">Clinic Pictures</span>
+                <label htmlFor="myfile">Select Images</label>
+                <input
+                  type="file"
+                  className="file-input"
+                  id="myfile"
+                  name="myfile"
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="map-part">
+          <DisplayMap
+            addressLatLong={[1.3521, 103.8198]}
+            mapBorderRadius={"0px 10px 10px 0px"}
+          />
+        </div>
       </div>
     </div>
   );
