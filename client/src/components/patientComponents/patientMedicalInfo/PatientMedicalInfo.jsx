10966716c78a02da1dd38657a4375c933c85babb
Hiperultimate <hiperultimatevegito@gmail.com>
2021-09-16 17:22:40 +0530

    Styled medical info and changed input box styling

--- client/src/components/patientComponents/patientMedicalInfo/PatientMedicalInfo.jsx
+++ client/src/components/patientComponents/patientMedicalInfo/PatientMedicalInfo.jsx
@@ -1,7 +1,66 @@
 import "./patientMedicalInfo.css";
+import InputField from "../../inputField/InputField.jsx";
+import DropdownList from "react-widgets/DropdownList";
+import MultiSelect from "../../multiSelect/MultiSelect.jsx";
 
-function PatientMedicalInfo(){
-    return(<div>Patient Medical Info</div>)
+function PatientMedicalInfo() {
+  return (
+    <div className="patient-medical-info">
+      <div class="medical-info-grid">
+        <div class="patient-weight">
+          <InputField
+            heading={"Weight"}
+            placeholder={"Enter your weight"}
+            type={"number"}
+            wrapperClass={"input-dimension"}
+            fieldName={"patient-weight"}
+            scaleText={"Kg"}
+            scaleTextPos={"right"}
+            setChange
+            value
+          />
+        </div>
+        <div class="patient-height">
+          <InputField
+            heading={"Height"}
+            placeholder={"Enter your height"}
+            type={"number"}
+            wrapperClass={"input-dimension"}
+            fieldName={"patient-height"}
+            scaleText={"cm"}
+            scaleTextPos={"right"}
+            setChange
+            value
+          />
+        </div>
+        <div class="patient-gender">
+          <span className="input-heading">Gender</span>
+          <DropdownList
+            placeholder={"Enter your gender"}
+            data={["Male", "Female", "Prefer not to say"]}
+            // onChange={(value) => setPatientGender(value)}
+          />
+        </div>
+        <div class="patient-bloodgroup">
+          <span className="input-heading">Bloodgroup</span>
+          <DropdownList
+            placeholder={"Enter your bloodgroup"}
+            data={["A", "A+", "AB+", "B", "B+", "B-"]}
+            // onChange={(value) => setBloodgroup(value)}
+          />
+        </div>
+        <div class="patient-allergies">
+          <span className="input-heading">Allergies</span>
+          <MultiSelect
+            options={["Egg", "Banana", "Dust", "Grass"]}
+            placeholder={"Enter allergies..."}
+            stateValue={[]}
+            handleState
+          />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default PatientMedicalInfo;
