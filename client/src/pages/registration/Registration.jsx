491a5a1a127d587e7286b2e3f28df03d63bb8329
Hiperultimate <hiperultimatevegito@gmail.com>
2021-09-17 14:39:06 +0530

    Made buttons to change forms

--- client/src/pages/registration/Registration.jsx
+++ client/src/pages/registration/Registration.jsx
@@ -1,17 +1,62 @@
 import "./registration.css";
+import { useState } from "react";
 import Navbar from "../../components/navbar/Navbar.jsx";
+import MainHeading from "../../components/mainHeading/MainHeading.jsx";
+import PatientRegister from "./registationForms/patientRegister/PatientRegister.jsx";
 import Footer from "../../components/footer/Footer.jsx";
 
-function Registration(){
-    return(
-        <>
-        <Navbar />
-        <div className="registration-page">
-            Registration Page
+function Registration() {
+  // 1 -> Doctor Form
+  // 2 -> Patient Form
+  const [formType, setFormType] = useState(2);
+
+  const someFunc = (e) => {
+    if (e.target.id === "doctor-btn") {
+      setFormType(1);
+    }
+    if (e.target.id === "patient-btn") {
+      setFormType(2);
+    }
+  };
+
+  return (
+    <>
+      <Navbar />
+      <div className="registration-page">
+        <MainHeading titleName={"Register"} />
+        <div className="select-form">
+          <button
+            style={
+              formType === 1
+                ? { backgroundColor: "var(--green-third)", color: "white" }
+                : { backgroundColor: "white" }
+            }
+            onClick={someFunc}
+            type="button"
+            className="select-btn doctorFormBtn global-box-shadow"
+            id="doctor-btn"
+          >
+            I'm a Doctor
+          </button>
+          <button
+            style={
+              formType === 2
+                ? { backgroundColor: "var(--green-third)", color: "white" }
+                : { backgroundColor: "white" }
+            }
+            onClick={someFunc}
+            type="button"
+            className="select-btn patientFormBtn global-box-shadow"
+            id="patient-btn"
+          >
+            I'm a Patient
+          </button>
         </div>
-        <Footer />
-        </>
-    );
+        <PatientRegister />
+      </div>
+      <Footer />
+    </>
+  );
 }
 
 export default Registration;
---
+++ client/src/pages/registration/registationForms/patientRegister/PatientRegister.jsx
@@ -0,0 +1,9 @@
+import "./patientRegister.css";
+
+function PatientRegister(){
+    return(
+        <div>Patient Register</div>
+    );
+}
+
+export default PatientRegister;
