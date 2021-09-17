5e7a524b6e40a6bcd0b9a3ecf18f6242671db188
Hiperultimate <hiperultimatevegito@gmail.com>
2021-09-17 12:43:30 +0530

    Setup registration page routing and base page

---
+++ client/src/pages/registration/Registration.jsx
@@ -0,0 +1,17 @@
+import "./registration.css";
+import Navbar from "../../components/navbar/Navbar.jsx";
+import Footer from "../../components/footer/Footer.jsx";
+
+function Registration(){
+    return(
+        <>
+        <Navbar />
+        <div className="registration-page">
+            Registration Page
+        </div>
+        <Footer />
+        </>
+    );
+}
+
+export default Registration;
