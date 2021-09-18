8f5b3f8a3f7b6d00721553b4e8d8b563b95ffefe
Hiperultimate <hiperultimatevegito@gmail.com>
2021-09-18 15:36:15 +0530

    Added medical history component in patient about page, and finalized the styling.

--- client/src/pages/patientAbout/PatientAbout.jsx
+++ client/src/pages/patientAbout/PatientAbout.jsx
@@ -5,6 +5,7 @@ import MainContainer from "../../components/mainContainer/MainContainer.jsx";
 import MainContHead from "../../components/mainContHead/MainContHead.jsx";
 import ContactInfo from "../../components/patientComponents/contactInfo/ContactInfo.jsx";
 import MedicalInfo from "../../components/patientComponents/medicalInfo/MedicalInfo.jsx";
+import MedicalHistory from "../../components/patientComponents/medicalHistory/MedicalHistory.jsx";
 import Footer from "../../components/footer/Footer.jsx";
 
 function PatientAbout() {
@@ -23,7 +24,7 @@ function PatientAbout() {
                 contactEmail={"alanokovTheDoctor@gmail.com"}
                 contactPhone={"981273591"}
                 contactAddress={"1293 Akaha police station"}
-                contactAddressMap={[56.1304, 106.3468]}
+                contactAddressMap={[28.6394, 77.3653]}
               />
             </div>,
             <MainContHead titleName="Medical Info" key={3} />,
@@ -35,6 +36,53 @@ function PatientAbout() {
                 patientHeight={"156"}
                 patientWeight={"95"}
                 patientAllergies={["Mangoes", "Potatoes", "Rice", "Salt"]}
+              />
+            </div>,
+            <MainContHead titleName="Medical History" key={5} />,
+            <div style={{ paddingBottom: "0em" }} key={6}>
+              <MedicalHistory
+                medicalHistory={[
+                  {
+                    DiseaseName: "Blood Cancer",
+                    CureTimeTaken: "21 Sept, 2021 - 30 December 2022",
+                    Comments: "Severe cases of blood ...",
+                    Medication: [
+                      {
+                        medName: "Chemo",
+                        medFrequency: "Once a day",
+                        medDuration: "31 Oct - 30 Dec",
+                      },
+                      {
+                        medName: "Pain Killers",
+                        medFrequency: "Once a week",
+                        medDuration: "15 Oct - 30 Dec",
+                      },
+                      {
+                        medName: "Drug 2",
+                        medFrequency: "3 times a day",
+                        medDuration: "31 Sept - 30 Dec",
+                      },
+                    ],
+                  },
+                  {
+                    DiseaseName: "Common Cold",
+                    CureTimeTaken: "25 Jan, 2022 - 30 February 2022",
+                    Comments: "Winter cold, minor coughs.",
+                    Medication: [
+                      {
+                        medName: "Coldo",
+                        medFrequency: "Three times a day",
+                        medDuration: "25 Jan - 29 Jan",
+                      },
+                      {
+                        medName: "Sleeping Pills",
+                        medFrequency: "At night",
+                        medDuration: "15 Feb - 19 Feb",
+                      },
+                    ],
+                  },
+                  
+                ]}
               />
             </div>,
           ]}
