703dbda2758977a4d4e8c4b5d8625b050d76339b
Hiperultimate <hiperultimatevegito@gmail.com>
2021-09-20 15:44:13 +0530

    No users found message on empty chat list.

--- client/src/pages/chat/Chat.jsx
+++ client/src/pages/chat/Chat.jsx
@@ -18,58 +18,62 @@ const DisplayType = Object.freeze({
 
 function Chat() {
   const [displayType, setDisplayType] = useState(DisplayType.PATIENT);
-    const fetchChatUsers = [
-      {
-        userName: "Alanakov Bavonoichi",
-        unreadMessageCount: "5",
-        displayInfo: "Common Cold",
-      },
-      {
-        userName: "Suzukaze Nozuaki",
-        unreadMessageCount: "103",
-        displayInfo: "Uncommon Cold",
-      },
-      {
-        userName: "Rasnek Poker",
-        unreadMessageCount: "0",
-        displayInfo: "Rare Cold",
-      },
-      {
-        userName: "Fantasma Rais",
-        unreadMessageCount: "0",
-        displayInfo: "Epic Cold",
-      },
-      {
-        userName: "Zankarea Zeus",
-        unreadMessageCount: "21",
-        displayInfo: "Legendary Cold",
-      },
-    ];
+  const fetchChatUsers = [
+    {
+      userName: "Alanakov Bavonoichi",
+      unreadMessageCount: "5",
+      displayInfo: "Common Cold",
+    },
+    {
+      userName: "Suzukaze Nozuaki",
+      unreadMessageCount: "103",
+      displayInfo: "Uncommon Cold",
+    },
+    {
+      userName: "Rasnek Poker",
+      unreadMessageCount: "0",
+      displayInfo: "Rare Cold",
+    },
+    {
+      userName: "Fantasma Rais",
+      unreadMessageCount: "0",
+      displayInfo: "Epic Cold",
+    },
+    {
+      userName: "Zankarea Zeus",
+      unreadMessageCount: "21",
+      displayInfo: "Legendary Cold",
+    },
+  ];
 
   // Patient view of doctor chat entry example
-//   const fetchChatUsers = [
-//     {
-//       userName: "Alanakov Bavonoichi",
-//       unreadMessageCount: "5",
-//       displayInfo: ["Surgeries", "Anesthetics", "Valorant", "Punjabi"],
-//     },
-//   ];
+  //   const fetchChatUsers = [
+  //     {
+  //       userName: "Alanakov Bavonoichi",
+  //       unreadMessageCount: "5",
+  //       displayInfo: ["Surgeries", "Anesthetics", "Valorant", "Punjabi"],
+  //     },
+  //   ];
 
   return (
     <>
       <Navbar isFixed={true} />
       <div className="chat-container">
-        <div className="chat-nav">
-          {fetchChatUsers.map((chatUsers) => {
-            return (
-              <SelectChat
-                userName={chatUsers.userName}
-                unreadMessageCount={chatUsers.unreadMessageCount}
-                displayInfo={chatUsers.displayInfo}
-                viewType={displayType}
-              />
-            );
-          })}
+        <div className="chat-nav" style={fetchChatUsers.length <= 0 ? {textAlign: "center"} : {}}>
+          {fetchChatUsers.length <= 0 ? (
+            <span className="empty-chat-nav">No users found...</span>
+          ) : (
+            fetchChatUsers.map((chatUsers) => {
+              return (
+                <SelectChat
+                  userName={chatUsers.userName}
+                  unreadMessageCount={chatUsers.unreadMessageCount}
+                  displayInfo={chatUsers.displayInfo}
+                  viewType={displayType}
+                />
+              );
+            })
+          )}
         </div>
         <div className="chat-text-area">Chat text area</div>
         <div className="set-prescription">Give Prescription</div>
