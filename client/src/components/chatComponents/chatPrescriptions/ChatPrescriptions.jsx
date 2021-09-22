c6c0d195633a52b76d8dfd85f72fa02be1560f51
Hiperultimate <hiperultimatevegito@gmail.com>
2021-09-22 10:58:25 +0530

    Completed prescription styling

--- client/src/components/chatComponents/chatPrescriptions/ChatPrescriptions.jsx
+++ client/src/components/chatComponents/chatPrescriptions/ChatPrescriptions.jsx
@@ -2,7 +2,12 @@ import "./chatPrescriptions.css";
 import PlusSvg from "../../../svgs/plus.svg";
 import PrescriptionCard from "./prescriptionCard/PrescriptionCard.jsx";
 
-function ChatPrescriptions() {
+const DisplayType = Object.freeze({
+  DOCTOR: 1,
+  PATIENT: 2,
+});
+
+function ChatPrescriptions({ displayType }) {
   return (
     <div>
       <div className="prescription-heading-container">
@@ -10,21 +15,43 @@ function ChatPrescriptions() {
         <span className="prescription-line-seperator" />
       </div>
       <div className="prescription-container">
-        <PrescriptionCard />
+        {/* Map function after fetching data: here*/}
+        <PrescriptionCard
+          medicineName={"Painkiller 2"}
+          frequency={"1 Cup / 50ml"}
+          quantity={"Once a day"}
+          duration={"31 Sep - 5 Oct"}
+          displayType={displayType}
+        />
+        <PrescriptionCard
+          medicineName={"Afro Maker"}
+          frequency={"5 Tablets"}
+          quantity={"Once a day"}
+          duration={"31 Sep - 5 Oct"}
+          displayType={displayType}
+        />
+        <PrescriptionCard
+          medicineName={"Sleep Medicine"}
+          frequency={"3 Cups with 200ml Water"}
+          quantity={"Twice a day"}
+          duration={"31 Sep - 5 Oct"}
+          displayType={displayType}
+        />
       </div>
+      {displayType === DisplayType.DOCTOR && (
+        <div className="session-option-container">
+          <span className="session-line-seperator" />
 
-      <div className="session-option-container">
-        <span className="session-line-seperator" />
-
-        <div className="chat-options">
-          <button className="close-session-btn global-box-shadow">
-            <span>Close Session</span>
-          </button>
-          <button className="add-prescription-btn global-box-shadow">
-            <img src={PlusSvg} alt="plus-svg" />
-          </button>
+          <div className="chat-options">
+            <button className="close-session-btn global-box-shadow">
+              <span>Close Session</span>
+            </button>
+            <button className="add-prescription-btn global-box-shadow">
+              <img className="plus-icon-svg" src={PlusSvg} alt="plus-svg" />
+            </button>
+          </div>
         </div>
-      </div>
+      )}
     </div>
   );
 }
