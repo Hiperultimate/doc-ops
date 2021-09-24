96f6322f9d1d9d798be516ef55ce6bc44e84a5b6
Hiperultimate <hiperultimatevegito@gmail.com>
2021-09-24 17:30:08 +0530

    Improved add prescription styling and created map operation for prescription card.

--- client/src/components/chatComponents/chatPrescriptions/ChatPrescriptions.jsx
+++ client/src/components/chatComponents/chatPrescriptions/ChatPrescriptions.jsx
@@ -7,7 +7,36 @@ const DisplayType = Object.freeze({
   PATIENT: 2,
 });
 
-function ChatPrescriptions({ displayType , setCloseSessionState ,setAddPrescriptionState}) {
+function ChatPrescriptions({
+  displayType,
+  setCloseSessionState,
+  setAddPrescriptionState,
+}) {
+  // Dummy data
+  let fetchPrescriptionData = [
+    {
+      medicineName: "Painkiller 2",
+      frequency: "1 Cup / 50ml",
+      quantity: "Once a day",
+      duration: "31 Sep - 5 Oct",
+      id: 1,
+    },
+    {
+      medicineName: "Afro Maker",
+      frequency: "5 Tablets",
+      quantity: "Once a day",
+      duration: "31 Sep - 5 Oct",
+      id: 2,
+    },
+    {
+      medicineName: "Sleep Medicine",
+      frequency: "3 Cups with 200ml Water",
+      quantity: "Twice a day",
+      duration: "31 Sep - 5 Oct",
+      id: 3,
+    },
+  ];
+
   return (
     <div>
       <div className="prescription-heading-container">
@@ -15,38 +44,34 @@ function ChatPrescriptions({ displayType , setCloseSessionState ,setAddPrescript
         <span className="prescription-line-seperator" />
       </div>
       <div className="prescription-container">
-        {/* Map function after fetching data: here*/}
-        <PrescriptionCard
-          medicineName={"Painkiller 2"}
-          frequency={"1 Cup / 50ml"}
-          quantity={"Once a day"}
-          duration={"31 Sep - 5 Oct"}
-          displayType={displayType}
-        />
-        <PrescriptionCard
-          medicineName={"Afro Maker"}
-          frequency={"5 Tablets"}
-          quantity={"Once a day"}
-          duration={"31 Sep - 5 Oct"}
-          displayType={displayType}
-        />
-        <PrescriptionCard
-          medicineName={"Sleep Medicine"}
-          frequency={"3 Cups with 200ml Water"}
-          quantity={"Twice a day"}
-          duration={"31 Sep - 5 Oct"}
-          displayType={displayType}
-        />
+        {fetchPrescriptionData.map((prescription) => {
+          return (
+            <PrescriptionCard
+              medicineName={prescription.medicineName}
+              frequency={prescription.frequency}
+              quantity={prescription.quantity}
+              duration={prescription.duration}
+              displayType={displayType}
+              key={prescription.id}
+            />
+          );
+        })}
       </div>
       {displayType === DisplayType.DOCTOR && (
         <div className="session-option-container">
           <span className="session-line-seperator" />
 
           <div className="chat-options">
-            <button onClick={() => setCloseSessionState(true)} className="close-session-btn global-box-shadow">
+            <button
+              onClick={() => setCloseSessionState(true)}
+              className="close-session-btn global-box-shadow"
+            >
               <span>Close Session</span>
             </button>
-            <button onClick={()=> setAddPrescriptionState(true)} className="add-prescription-btn global-box-shadow">
+            <button
+              onClick={() => setAddPrescriptionState(true)}
+              className="add-prescription-btn global-box-shadow"
+            >
               <img className="plus-icon-svg" src={PlusSvg} alt="plus-svg" />
             </button>
           </div>
--- client/src/components/chatComponents/chatPrescriptions/addPrescription/AddPrescription.jsx
+++ client/src/components/chatComponents/chatPrescriptions/addPrescription/AddPrescription.jsx
@@ -5,61 +5,63 @@ function AddPrescription({ AddPrescriptionState }) {
   const { addPrescriptionState, setAddPrescriptionState } =
     AddPrescriptionState;
   return (
-    addPrescriptionState && <>
-      <div className="back-drop" />
-      <div className="add-prescription-container">
-        <img
+    addPrescriptionState && (
+      <>
+        <div className="back-drop" />
+        <div className="add-prescription-container">
+          <img
             onClick={() => setAddPrescriptionState(false)}
-          className="add-prescription-svg"
-          src={CrossBgSvg}
-          alt="cross-btn-svg"
-        />
-        <div class="create-prescription-grid">
-          <div class="amount-text">Amount:</div>
-          <div class="frequency-text">Frequency:</div>
-          <div class="duration-text">Duration:</div>
-          <div class="medicine-text">Medicine name:</div>
-          <div class="medicine-input">
-            <input
-              className="create-prescription-input"
-              type="text"
-              placeholder="Enter medicine name..."
-            />
-          </div>
-          <div class="amount-input">
-            <input
-              className="create-prescription-input"
-              type="text"
-              placeholder="Enter amount..."
-            />
-          </div>
-          <div class="frequency-input">
-            <input
-              className="create-prescription-input"
-              type="text"
-              placeholder="Enter frequency..."
-            />
-          </div>
-          <div class="duration-date-from">
-            <input
-              className="create-prescription-date-input"
-              type="date"
-            />
-          </div>
-          <div class="duration-date-to">
-            <input
-              className="create-prescription-date-input"
-              type="date"
-            />
-          </div>
-          <div class="create-prescription-btn">
-            <button className="global-box-shadow create-btn-right">
-              Create
-            </button>
+            className="add-prescription-svg"
+            src={CrossBgSvg}
+            alt="cross-btn-svg"
+          />
+          <div className="create-prescription-grid">
+            <div className="amount-text">Amount:</div>
+            <div className="frequency-text">Frequency:</div>
+            <div className="duration-text">Duration:</div>
+            <div className="medicine-text">Medicine name:</div>
+            <div className="medicine-input">
+              <input
+                className="create-prescription-input"
+                type="text"
+                placeholder="Enter medicine name..."
+              />
+            </div>
+            <div className="amount-input">
+              <input
+                className="create-prescription-input"
+                type="text"
+                placeholder="Enter amount..."
+              />
+            </div>
+            <div className="frequency-input">
+              <input
+                className="create-prescription-input"
+                type="text"
+                placeholder="Enter frequency..."
+              />
+            </div>
+            <div className="duration-date-from">
+              <input
+                className="create-prescription-date-input"
+                type="date"
+                required
+              />
+              <span>From </span>
+            </div>
+            <div className="duration-date-to">
+              <input className="create-prescription-date-input" type="date" />
+              <span>To </span>
+            </div>
+            <div className="create-prescription-btn">
+              <button className="global-box-shadow create-btn-right">
+                Create
+              </button>
+            </div>
           </div>
         </div>
-      </div>
-    </>
+      </>
+    )
   );
 }
 
