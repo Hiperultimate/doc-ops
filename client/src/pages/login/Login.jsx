8c15ab97d86dc2a1119e60e8988844af7d3f7649
Damandeep Singh Bawa <41473923+DamFlancoz@users.noreply.github.com>
2021-09-27 02:42:05 -0700

    setup up login page

--- client/src/pages/login/Login.jsx
+++ client/src/pages/login/Login.jsx
@@ -1,15 +1,72 @@
-import React from 'react';
 import "./login.css";
+import { useState } from "react";
+import InputField from "../../components/inputField/InputField.jsx";
 import TheLifeSavers from "../../svgs/The Lifesavers One on One.svg";
+import LifeSaversStethoscope from "../../svgs/The Lifesavers Stethoscope.svg";
+import LifeSaversAvatar from "../../svgs/Lifesavers Avatar.svg";
 
-function Login(){
-    return(
-        <div>
-            Login page component.
-            <img src={TheLifeSavers} alt="backgroud-img1" />
+function Login() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  return (
+    <div className="login-page">
+      <div className="welcome">
+        <div className="nav-brand">DocOps</div>
+        <div className="welcome-content">
+          <div className="welcome-heading">
+            Welcome to <strong>DocOps</strong>
+          </div>
+          <div className="welcome-sub-heading">
+            Get your medical appointment today!
+          </div>
+          <div>
+            <div className="floor-eclipse" />
+            <img
+              src={TheLifeSavers}
+              className="life-savers-svg"
+              alt="background-img1"
+            />
+            <img
+              src={LifeSaversStethoscope}
+              className="life-savers-stethoscope-svg"
+              alt="background-stethoscope"
+            />
+          </div>
         </div>
-    );
+      </div>
+      <div className="login-section">
+        <div className="login-content">
+          <img
+            src={LifeSaversAvatar}
+            className="life-savers-avatar-svg"
+            alt="avatar svg"
+          />
+          <InputField
+            placeholder={"Username"}
+            wrapperClass={"login-field"}
+            setChange={setUsername}
+            value={username}
+          />
+          <InputField
+            placeholder={"Password"}
+            wrapperClass={"login-field"}
+            type={"password"}
+            setChange={setPassword}
+            value={password}
+          />
+          <div className="login-link">Forgot Password?</div>
+          <button className={"login-field login-button"} type="submit">
+            Login
+          </button>
+          <div className="login-sub-text">
+            Don't have an account? &nbsp;
+            <span className="login-link"> Sign Up </span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
-
 
 export default Login;
