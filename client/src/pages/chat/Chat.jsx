d4025e987ffe3994469a32b59c78752bc242b001
Hiperultimate <hiperultimatevegito@gmail.com>
2022-02-05 16:59:37 +0530

    Fetching chatting currently chatting with users and updating views for user type.

--- client/src/pages/chat/Chat.jsx
+++ client/src/pages/chat/Chat.jsx
@@ -1,5 +1,6 @@
 import "./chat.css";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+
 import Navbar from "../../components/navbar/Navbar.jsx";
 import CloseSessionBox from "../../components/chatComponents/chatPrescriptions/closeSessionBox/CloseSessionBox.jsx";
 import SelectChat from "../../components/chatComponents/selectChat/SelectChat.jsx";
@@ -8,6 +9,10 @@ import TypingBar from "../../components/chatComponents/typingBar/TypingBar.jsx";
 import DisplayMessages from "../../components/chatComponents/displayMessages/DisplayMessages.jsx";
 import ChatPrescriptions from "../../components/chatComponents/chatPrescriptions/ChatPrescriptions.jsx";
 import AddPrescription from "../../components/chatComponents/chatPrescriptions/addPrescription/AddPrescription.jsx";
+
+import { doc, getDoc } from "firebase/firestore";
+import { db } from "../../firebase.js";
+import { useAuth } from "../../utils/contexts/AuthContext.js";
 
 const DisplayType = Object.freeze({
   DOCTOR: 1,
@@ -35,18 +40,58 @@ const DisplayType = Object.freeze({
 function Chat() {
   let chatID = 0;
 
+  const { currentUser, currentUserData } = useAuth();
+  const displayType = currentUserData.type === 1 ? DisplayType.DOCTOR : DisplayType.PATIENT;
   const [closeSessionState, setCloseSessionState] = useState(false);
   const [closeSessionDiagnosis, setCloseSessionDiagnosis] = useState("");
   const [closeSessionComments, setCloseSessionComments] = useState("");
   const [addPrescriptionState, setAddPrescriptionState] = useState(false);
-  const [displayType, setDisplayType] = useState(DisplayType.DOCTOR);
-  const fetchChatUsers = [
-    {
-      userName: "Alanakov Bavonoichi",
-      unreadMessageCount: "5",
-      displayInfo: ["Surgeries", "Anesthetics", "Valorant", "Punjabi"],
-    },
-  ];
+  const [fetchChatUsers, setFetchChatUsers] = useState([]);
+  // const fetchChatUsers = [
+  //   {
+  //     userName: "Alanakov Bavonoichi",
+  //     unreadMessageCount: "5",
+  //     displayInfo: ["Surgeries", "Anesthetics", "Valorant", "Punjabi"],
+  //   },
+  // ];
+
+  useEffect(() => {
+    async function fetchChatUserInfo(userUID) {
+      let fetchUserData = await getDoc(doc(db, "users", userUID));
+      const userInfo = fetchUserData.data();
+      let returnData = {};
+      if (userInfo.type === 1) {
+        let parsedData = {
+          userName: userInfo.doctorName,
+          unreadMessageCount: "5",
+          displayInfo: userInfo.specialization,
+        };
+        returnData = parsedData;
+      } else {
+        let parsedData = {
+          userName: userInfo.name,
+          unreadMessageCount: "5",
+          displayInfo: userInfo.diagnosis, // Discuss what to do here
+        };
+        returnData = parsedData;
+      }
+      return returnData;
+    }
+    async function fetchChattingWithUsers() {
+      let chattingWith = await getDoc(doc(db, "chattingWith", currentUser.uid));
+
+      let selectChatUser = [];
+      const chattingWithList = chattingWith.data().users;
+      for( let i = 0 ; i < chattingWithList.length; i++){
+        let chattingWithUser = await fetchChatUserInfo(chattingWithList[i]);
+        selectChatUser.push(chattingWithUser);
+      }
+      setFetchChatUsers(selectChatUser);
+      console.log(chattingWith.data().users);
+    }
+    fetchChattingWithUsers();
+  }, []);
+
 
   return (
     <>
