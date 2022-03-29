68aa2063716310c4345675fbdfca8591e52ee0c0
Hiperultimate <hiperultimatevegito@gmail.com>
2022-03-29 16:27:47 +0530

    Completed logic for closing prescription.

--- client/src/components/chatComponents/chatPrescriptions/closeSessionBox/CloseSessionBox.jsx
+++ client/src/components/chatComponents/chatPrescriptions/closeSessionBox/CloseSessionBox.jsx
@@ -3,7 +3,19 @@ import CrossBgSvg from "../../../../svgs/cross-bg.svg";
 
 import { useEffect, useState } from "react";
 
-import { collection, orderBy, query, getDocs, limit } from "firebase/firestore";
+import {
+  doc,
+  collection,
+  orderBy,
+  query,
+  getDocs,
+  limit,
+  setDoc,
+  arrayUnion,
+  updateDoc,
+  deleteDoc,
+  arrayRemove,
+} from "firebase/firestore";
 import { db } from "../../../../firebase.js";
 
 function CloseSessionBox({
@@ -40,6 +52,90 @@ function CloseSessionBox({
 
   const handleInputChange = (e, setState) => {
     setState(e.target.value);
+  };
+
+  // Takes in prescriptionList and accesses its objects
+  const databaseDateToStringDate = (dbObj) => {
+    let medicineFromDate = new Date(
+      dbObj.prescriptionDetails.medicineDurationFrom.seconds * 1000
+    );
+    let medicineToDate = new Date(
+      dbObj.prescriptionDetails.medicineDurationTo.seconds * 1000
+    );
+    let displayDate =
+      medicineFromDate.getDate().toString() +
+      " " +
+      intToDate[medicineFromDate.getMonth() + 1] +
+      " - " +
+      medicineToDate.getDate().toString() +
+      " " +
+      intToDate[medicineToDate.getMonth() + 1]; // getMonth uses 0 based index, so we use +1
+
+    return displayDate;
+  };
+
+  const closeSession = async (e) => {
+    e.preventDefault();
+
+    const storeData = {};
+    storeData.DiseaseName = closeSessionDiagnosis;
+    storeData.Comments = closeSessionComments;
+    storeData.Medication = []; // list of medicine objects
+    prescriptionList.map((prescriptionData) => {
+      let displayDate = databaseDateToStringDate(prescriptionData);
+      let medicineObj = {};
+      medicineObj["medicineName"] =
+        prescriptionData.prescriptionDetails.medicineName;
+      medicineObj["medicineFrequency"] =
+        prescriptionData.prescriptionDetails.medicineFrequency;
+      medicineObj["medicineDuration"] = displayDate;
+      storeData.Medication.push(medicineObj);
+    });
+    let saveObj = {};
+    saveObj["medicalHistory"] = [storeData];
+
+    await setDoc(
+      doc(db, "users", selectedUserUID),
+      { medicalHistory: arrayUnion(storeData) },
+      { merge: true }
+    );
+
+    // Delete chattingWith currentUserUID and selectedUserUID then reload page
+
+    // Deleting chattingWith collection data
+    // For selectedUserUID
+    const selectedUserChattingWithRef = doc(db, "chattingWith",selectedUserUID);
+    await updateDoc(selectedUserChattingWithRef, {
+      unreadMessage: arrayRemove(currentUserUID),
+      users: arrayRemove(currentUserUID),
+    });
+  
+    // // For currentUserUID
+    const currenUserChattingWithRef = doc(db, "chattingWith",currentUserUID);
+    await updateDoc(currenUserChattingWithRef, {
+      unreadMessage: arrayRemove(selectedUserUID),
+      users: arrayRemove(selectedUserUID),
+    });
+  
+    // Deleting every document inside sessions/chatRoomString/chat and sessions/chatRoomString/prescription
+    const chatRoomString =
+      currentUserUID > selectedUserUID
+        ? `${currentUserUID + selectedUserUID}`
+        : `${selectedUserUID + currentUserUID}`;
+    
+    const chatSnapshot = await getDocs(collection(db, "sessions",chatRoomString, "chat"));
+    chatSnapshot.forEach(async (chatDoc) => {
+      const chatDocumentInstance = doc(db, "sessions",chatRoomString, "chat", chatDoc.id);
+      await deleteDoc(chatDocumentInstance);
+    });
+
+    const prescriptionSnapshot = await getDocs(collection(db, "sessions",chatRoomString, "prescription"));
+    prescriptionSnapshot.forEach(async (presDoc) => {
+      const chatDocumentInstance = doc(db, "sessions",chatRoomString, "prescription", presDoc.id);
+      await deleteDoc(chatDocumentInstance);
+    });
+
+    window.location.reload(false);
   };
 
   const getChatCreatedDate = async () => {
@@ -81,100 +177,99 @@ function CloseSessionBox({
   return (
     closeSessionState && (
       <>
-        <div className="back-drop" />
-        <div className="close-session-box">
-          <img
-            onClick={() => setCloseSessionState(false)}
-            className="close-session-svg"
-            src={CrossBgSvg}
-            alt="cross-btn-svg"
-          />
-          <div className="close-session-grid">
-            <div className="diagnosis-text">Diagnosis:</div>
-            <div className="comments-text">Comments:</div>
-            <div className="medication-text">Medication:</div>
-            <div className="comment-input-container">
-              <input
-                onChange={(e) => handleInputChange(e, setCloseSessionComments)}
-                value={closeSessionComments}
-                className="close-session-input"
-                type="text"
-                placeholder="Additional notes..."
-              />
-            </div>
-            <div className="diagnosis-input-container">
-              <input
-                onChange={(e) => handleInputChange(e, setCloseSessionDiagnosis)}
-                value={closeSessionDiagnosis}
-                className="close-session-input"
-                type="text"
-                placeholder="Enter diagnosis..."
-              />
-            </div>
-            <div className="end-date-text">
-              {startDate} - {endDate}
-            </div>
-            <div className="medication-list">
-              <table>
-                <tbody>
-                  <tr>
-                    <th>Name</th>
-                    <th>Frequency</th>
-                    <th>Amount</th>
-                    <th>Duration</th>
-                  </tr>
-                  {prescriptionList !== null &&
-                    prescriptionList.map((prescriptionObj, index) => {
-                      let medicineFromDate = new Date(
-                        prescriptionObj.prescriptionDetails.medicineDurationFrom
-                          .seconds * 1000
-                      );
-                      let medicineToDate = new Date(
-                        prescriptionObj.prescriptionDetails.medicineDurationTo
-                          .seconds * 1000
-                      );
-                      let displayDate =
-                        medicineFromDate.getDate().toString() +
-                        " " +
-                        intToDate[medicineFromDate.getMonth() + 1] +
-                        " - " +
-                        medicineToDate.getDate().toString() +
-                        " " +
-                        intToDate[medicineToDate.getMonth() + 1]; // getMonth uses 0 based index, so we use +1
-                      return (
-                        <tr key={index}>
-                          <td>
-                            {prescriptionObj.prescriptionDetails.medicineName}
-                          </td>
-                          <td>
-                            {
-                              prescriptionObj.prescriptionDetails
-                                .medicineFrequency
-                            }
-                          </td>
-                          <td>
-                            {prescriptionObj.prescriptionDetails.medicineAmount}
-                          </td>
-                          <td>{displayDate}</td>
-                        </tr>
-                      );
-                    })}
-                </tbody>
-              </table>
-            </div>
-            <div className="close-without-diagnosis-btn">
-              <button
-                onClick={() => setCloseSessionState(false)}
-                className="global-box-shadow diagnosis-btn-right"
-              >
-                Close Session Without Diagnosis
-              </button>
-            </div>
-            <div className="session-close-btn">
-              <button className="global-box-shadow">Close Session</button>
+        <form onSubmit={closeSession}>
+          <div className="back-drop" />
+          <div className="close-session-box">
+            <img
+              onClick={() => setCloseSessionState(false)}
+              className="close-session-svg"
+              src={CrossBgSvg}
+              alt="cross-btn-svg"
+            />
+            <div className="close-session-grid">
+              <div className="diagnosis-text">Diagnosis:</div>
+              <div className="comments-text">Comments:</div>
+              <div className="medication-text">Medication:</div>
+              <div className="comment-input-container">
+                <input
+                  onChange={(e) =>
+                    handleInputChange(e, setCloseSessionComments)
+                  }
+                  value={closeSessionComments}
+                  className="close-session-input"
+                  type="text"
+                  placeholder="Additional notes..."
+                  required
+                />
+              </div>
+              <div className="diagnosis-input-container">
+                <input
+                  onChange={(e) =>
+                    handleInputChange(e, setCloseSessionDiagnosis)
+                  }
+                  value={closeSessionDiagnosis}
+                  className="close-session-input"
+                  type="text"
+                  placeholder="Enter diagnosis..."
+                  required
+                />
+              </div>
+              <div className="end-date-text">
+                {startDate} - {endDate}
+              </div>
+              <div className="medication-list">
+                <table>
+                  <tbody>
+                    <tr>
+                      <th>Name</th>
+                      <th>Frequency</th>
+                      <th>Amount</th>
+                      <th>Duration</th>
+                    </tr>
+                    {prescriptionList !== null &&
+                      prescriptionList.map((prescriptionObj, index) => {
+                        let displayDate =
+                          databaseDateToStringDate(prescriptionObj);
+                        return (
+                          <tr key={index}>
+                            <td>
+                              {prescriptionObj.prescriptionDetails.medicineName}
+                            </td>
+                            <td>
+                              {
+                                prescriptionObj.prescriptionDetails
+                                  .medicineFrequency
+                              }
+                            </td>
+                            <td>
+                              {
+                                prescriptionObj.prescriptionDetails
+                                  .medicineAmount
+                              }
+                            </td>
+                            <td>{displayDate}</td>
+                          </tr>
+                        );
+                      })}
+                  </tbody>
+                </table>
+              </div>
+              <div className="close-without-diagnosis-btn">
+                <button
+                  onClick={() => setCloseSessionState(false)}
+                  className="global-box-shadow diagnosis-btn-right"
+                >
+                  Close Session Without Diagnosis
+                </button>
+              </div>
+              <div className="session-close-btn">
+                <button type="submit" className="global-box-shadow">
+                  Close Session
+                </button>
+              </div>
             </div>
           </div>
-        </div>
+        </form>
       </>
     )
   );
